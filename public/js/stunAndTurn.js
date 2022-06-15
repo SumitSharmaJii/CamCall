a9a5e38a2565d6a5f5a7555b88b031e6ea0e3e96
Sumit <sumitsharma.ss97@gmail.com>
2022-06-15 15:20:16 +0530

    --version 1.0.0

--- public/js/stunAndTurn.js
+++ public/js/stunAndTurn.js
@@ -1,23 +1,19 @@
 iceServersList = {
 
-  iceServers: [
-    {
-      urls: "stun:openrelay.metered.ca:80",
-    },
-    {
-      urls: "turn:openrelay.metered.ca:80",
-      username: "openrelayproject",
-      credential: "openrelayproject",
-    },
-    {
-      urls: "turn:openrelay.metered.ca:443",
-      username: "openrelayproject",
-      credential: "openrelayproject",
-    },
-    {
-      urls: "turn:openrelay.metered.ca:443?transport=tcp",
-      username: "openrelayproject",
-      credential: "openrelayproject",
-    },
-  ]
+    iceServers: [
+        { urls: [ "stun:bn-turn1.xirsys.com" ]
+        },
+
+        { username: "R_P7E8V4yTU8mfsIOJvtlMYxINwsBZo5RaazzcAGF8Sro37AK0yLSkb06I45JiX0AAAAAGKppnZEZWVwYWxp",
+          credential: "a6658ccc-ec8d-11ec-aa17-0242ac140004",   
+          urls: ["turn:bn-turn1.xirsys.com:80?transport=udp",
+                "turn:bn-turn1.xirsys.com:3478?transport=udp",
+                "turn:bn-turn1.xirsys.com:80?transport=tcp",
+                "turn:bn-turn1.xirsys.com:3478?transport=tcp",
+                "turns:bn-turn1.xirsys.com:443?transport=tcp",
+                "turns:bn-turn1.xirsys.com:5349?transport=tcp"   
+                ]
+          }
+        
+        ]
 }
