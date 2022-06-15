77711b8e301d590fb48402d846428995604df17b
Sumit <sumitsharma.ss97@gmail.com>
2022-06-15 14:15:28 +0530

    updated stun and turn servers

---
+++ public/js/stunAndTurn.js
@@ -0,0 +1,23 @@
+iceServersList = {
+
+  iceServers: [
+    {
+      urls: "stun:openrelay.metered.ca:80",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443?transport=tcp",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+  ]
+}
